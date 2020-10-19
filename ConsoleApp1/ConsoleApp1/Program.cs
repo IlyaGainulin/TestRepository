68f2bbfe5c76a8edb6c70dc3b48faf5a39086fff
ilya gainulin <gainulin@viva64.com>
2020-10-19 10:02:47 +0300

    added git ignore file

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
        
         static void Main(string[] args)
         {
-            int a = 100;
+            int a = 500;
             if (a == a)
                 Console.WriteLine();
         }
